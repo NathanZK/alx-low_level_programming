60f58a5df2df58aa6722421bb2490a688665ad7e
Nathan <nathanzelalem100@gmail.com>
2022-07-01 13:03:48 -0700

    structures

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
+
