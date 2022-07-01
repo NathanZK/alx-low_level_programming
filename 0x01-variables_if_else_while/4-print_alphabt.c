d0d88373fa4d3ff9731d1e1276c5d3783536c7c8
Nathan <nathanzelalem100@gmail.com>
2022-07-01 12:59:27 -0700

    structures

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int char i = 'a';
+
+	for (; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
