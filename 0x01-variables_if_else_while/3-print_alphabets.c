a6ca75d349669a2424d629d07d9b388966b42364
Nathan <nathanzelalem100@gmail.com>
2022-07-01 12:55:35 -0700

    structures

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i = 'a';
+	char j = 'A';
+
+	for (; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (; j <= 'Z'; j++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
