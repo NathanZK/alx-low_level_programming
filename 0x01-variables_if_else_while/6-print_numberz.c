295ec77e74a16b2793ed0140464fb6a17dd3bc7d
Nathan <nathanzelalem100@gmail.com>
2022-07-01 13:11:09 -0700

    structures

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+	}
+	putchar('\');
+	return (0);
+}
+
