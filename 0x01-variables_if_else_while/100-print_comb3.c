6c9e7607bce1b10a3785e35b5b33ebd7794ea4a3
Nathan <nathanzelalem100@gmail.com>
2022-07-01 18:15:12 -0700

    x

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (; i < 10; i++)
+	{
+		j = 0;
+		for (; j < 10; j++)
+		{
+			if (i < j)
+			{
+				putchar(i % 10 + '0');
+				putchar(j % 10 + '0');
+
+				if (i == 8 && j == 9)
+				{
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
