458c6ee2013332ee12b92f09307f2765a7af7453
Nathan <nathanzelalem100@gmail.com>
2022-07-05 15:41:25 -0700

    funtions

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* _natural - sum of multiples of 3 and 5
+*
+*/
+void _natural(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			sum += i;
+		else if (i % 3 == 0)
+			sum += i;
+		else if (i % 5 == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+}
+
+
+
+
+
+
+
