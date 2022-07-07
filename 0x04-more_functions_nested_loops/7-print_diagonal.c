f5341a99025ac9b177def23ae157170baa18e8ca
Nathan <nathanzelalem100@gmail.com>
2022-07-07 05:12:09 -0700

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_diagonal - prints diagonal line
+* @n: integer argument
+*/
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
