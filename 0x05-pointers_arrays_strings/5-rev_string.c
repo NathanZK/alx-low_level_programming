419d6489035df9a5fc64f7e4a9bba08cd2a3d1ee
Nathan <nathanzelalem100@gmail.com>
2022-07-11 06:17:27 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,5 +8,13 @@
 void rev_string(char *s)
 {
 
-	*s = (char)strrev(s);
+	int i, len, temp;
+
+	len = strlen(s);
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
 }
