6bc592db59559185cd11681e63807d3270dea096
Nathan <nathanzelalem100@gmail.com>
2022-07-11 08:21:29 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+	char listi[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
+
+return 0;
