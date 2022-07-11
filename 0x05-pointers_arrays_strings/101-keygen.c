6b223ff5cb059987d99921a3611aa61cc3372ab9
Nathan <nathanzelalem100@gmail.com>
2022-07-11 08:55:46 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
 
-int main() {
+	int i;
+	char alphabet[] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+	char pass[15];
 
-
+	srand(time);
+	for (i = 0; i < 15; i++)
+	{
+		pass[i] = alphabet[rand() % strlen(alphabet)];
+		printf("%c", pass[i]);
+	}
 return (0);
 }
