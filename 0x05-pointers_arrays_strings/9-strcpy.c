a664b87a5155d0d7d2a8cef88c94a4fd80c60ba0
Nathan <nathanzelalem100@gmail.com>
2022-07-11 07:03:26 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strcpy -  copies string
+* @src: parameter 1
+* @dest: parameter 2
+* Return: char
+*/
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
