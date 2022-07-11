e24812d1e6a515849b3f340233ff3dc73da9eb22
Nathan <nathanzelalem100@gmail.com>
2022-07-11 06:56:32 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+* print_array -  prints array
+* @a: parameter 1
+* @n: parameter 2
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
