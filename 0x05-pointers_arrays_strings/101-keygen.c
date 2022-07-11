b1fae325134a3c32895b016b1f8d5f457ba021df
Nathan <nathanzelalem100@gmail.com>
2022-07-11 09:31:34 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,36 +5,20 @@
 
 int main() {
 
-char a[10],c[10];
-    int i,j,k=0;
-    printf("#Keygen by b44nz0r\n\n");
+int i;
+char alphabet[] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
 
-    while (k <5 || k >=10)
-    {
-          if (k !=0)
-             printf("\nThe username length should be 5 to 10 alphabets\n");
+srand(time(0));
 
-         printf("enter username: ");
-         scanf("%s",a);
-         k = strlen(a);
-    }
+char pass[15];
+for (i = 0; i < 15; i++)
+{
+pass[i] = alphabet[rand() % strlen(alphabet)];
+printf("%c", pass[i]);
+}
 
-    i = k-1;
-    j = 0;
 
-    while (i >= 0)
-    {
-          c[j] = a[i]+i;
-          i--;
-          j++;
-    }
 
-    c[j] = 0;
-    printf("\nThe password is %s\n",c);
-    printf("\nHit Enter to Exit\n");
-    getchar();
-    getchar();
-
-	return(0);
+return 0;
 }
 
