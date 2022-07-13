95313fc9b6b85b2df82aa4a02d7980ec6ad49d41
Nathan <nathanzelalem100@gmail.com>
2022-07-13 12:09:59 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,12 +9,28 @@
 char *cap_string(char *a)
 {
 	int i;
+	int count = 0;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		if (a[i] >= 97 && a[i] <= 122 && a[i - 1] < 48)
-			a[i] = a[i] - 32;
+		if (a[i] >= 97 && a[i] <= 122)
+		a[i] = a[i] - 32;
 	}
+
+	for (count = 0; a[count] != '\0'; count++)
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (a[count] == sep_words[i])
+			{
+				if (a[count + 1] >= 97 && a[count + 1] <= 122)
+					a[count + 1] = a[count + 1] - 32;
+					break;
+			}
+		}
+	}
+
 
 	return (a);
 }
