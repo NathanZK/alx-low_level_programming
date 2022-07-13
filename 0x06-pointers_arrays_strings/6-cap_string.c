1e01e2771d1ea9a570fcafd5693f213074cac205
Nathan <nathanzelalem100@gmail.com>
2022-07-13 11:53:42 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *a)
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		if (a[i] >= 97 && a[i] <= 122 && a[i - 1] < 64)
+		if (a[i] >= 97 && a[i] <= 122 && a[i - 1] < 48)
 			a[i] = a[i] - 32;
 	}
 
