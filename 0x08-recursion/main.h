41654ca8f600f643312651ebf97ebe45544d0781
Nathan <nathanzelalem100@gmail.com>
2022-07-20 12:22:24 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,7 +10,9 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt(int n, int i);
 int is_prime_number(int n);
-
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
 
 #endif /* MAIN_H */
