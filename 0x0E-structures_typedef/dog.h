bbbbf5e047c501fc70ecd948bef13fe6178ecfd6
Nathan <nathanzelalem100@gmail.com>
2022-08-01 09:20:50 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+* struct dog - dog
+* @name: name
+* @age: age
+* @owner: owner
+* Description: dog details
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
+
