b86614d7650e7efbb2447e2d4410dae664072c7c
Nathan <nathanzelalem100@gmail.com>
2022-08-04 04:32:44 -0700

    variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,14 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
+#include <stdio.h>
 #include <stdarg.h>
 
-int sum_them_all(const unsigned int n, ...);
+iint sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 
 
 #endif /* VARIADIC_FUNCTIONS */
